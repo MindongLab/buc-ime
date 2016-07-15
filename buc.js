6132d6c89aa690e3d687f00a59b162e75705b009
Foochow Missionary <foochow.missionary@gmail.com>
2016-07-15 20:02:03 +1000

    buc

---
+++ buc.js
@@ -0,0 +1,22 @@
+var d;
+// All vowel combinations
+
+['a', 'ia', 'ua', 'a̤', 'ie',
+'o̤', 'io', 'uo', 'e̤', 'ae̤', 
+'au', 'eu', 'aiu', 'ieu', 'iu',
+'oi', 'o̤i', 'ai', 'uai', 'uoi', 'ui',
+'i', 'e', 'u', 'o', 'ṳ', 'e̤ṳ',
+'ah', 'iah', 'uah', 'a̤h', 'ieh', 'o̤h',
+'ioh', 'uoh', 'e̤h']
+
+
+
+// Key mapping (without tones)
+var keymap = 
+{'a' : ['a','a̤'],
+'b': ['b'],
+'e' : ['e','e̤'],
+'o' : ['o','o̤'],
+'u' : ['u','ṳ'],
+}
+
